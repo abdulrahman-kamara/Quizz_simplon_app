51b461aa778fcf75e25d6c3d730909287ff7a41d
sines210 <sines210@gmail.com>
2021-03-24 15:17:58 +0100

    commit

---
+++ resources/views/components/footer.php
@@ -0,0 +1,5 @@
+        <footer class="foot">
+             <div class="footerDiv">
+                <a href="https://simplon.co/"> <img src="{{ asset('img/logoFoot.png') }}" alt="logo-simplon" id="logo-simplon"></a>
+             </div>
+         </footer>
