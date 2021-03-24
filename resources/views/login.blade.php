8823c06146c49b0dec9a503004d686ce2af1f86a
Monocure <Ida.b.zerkani@outlook.fr>
2021-03-24 16:52:52 +0100

    login template created

---
+++ resources/views/login.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Quizz Simplon</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../css/app.css">
+
+    </head>
+    <body>
+      
+    <main>
+
+    <div id="container">
+        <ul>
+            <li><img src="{{ asset('img/logoFoot.png') }}" alt="logo-simplon" id="logo-simplon"></li>
+            <li><a href="">Connexion</a></li>
+            <li><a href="">Inscription</a></li>
+        </ul>
+        <h1>Bienvenue sur le site Simplon Quizz</h1>
+        <p>Lorem Upsum</p>
+    </div>
+
+</main>
+
+    
+    </body>
+   
+</html>
+
+
