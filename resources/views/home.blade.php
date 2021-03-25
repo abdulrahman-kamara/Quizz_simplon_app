ef8cf96936454970c68ff48fd4f20abb5341e6e1
Monocure <Ida.b.zerkani@outlook.fr>
2021-03-25 15:59:23 +0100

    created templates

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+<main>
+
+    <div id="container">
+        <ul>
+            <li><img src="{{ asset('img/logoFoot.png') }}" alt="logo-simplon" id="logo-simplon"></li>
+            <li><a href="">Connexion</a></li>
+            <li><a href="">Inscription</a></li>
+        </ul>
+        <h1>Bienvenue sur le site Simplon Quizz</h1>
+        <p>Lorem Upsum</p>
+    </div>
+
+</main>
+
+
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,35 +1,19 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<main>
+  <img src="{{ asset('img/logoFoot.png') }}" alt="logo-simplon" id="logo-simplon">
+  <form>
 
-        <title>Quizz Simplon</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../css/app.css">
-
-    </head>
-    <body>
-      
-    <main>
-
-    <div id="container">
-        <ul>
-            <li><img src="{{ asset('img/logoFoot.png') }}" alt="logo-simplon" id="logo-simplon"></li>
-            <li><a href="">Connexion</a></li>
-            <li><a href="">Inscription</a></li>
-        </ul>
-        <h1>Bienvenue sur le site Simplon Quizz</h1>
-        <p>Lorem Upsum</p>
+    <div class="input-group">
+      <label for="email">E-mail</label>
+      <input id="email" name="email" />
     </div>
 
+    <div class="input-password">
+      <label for="password">Password</label>
+      <input id="password" name="password" type="password" />
+    </div>
+
+      <h3>Si vous n'avez pas de compte</h3>
+      <a href="">Inscrivez-vous</a>
+      <button type="submit">Login</button>
+  </form>
 </main>
-
-    
-    </body>
-   
-</html>
-
-
